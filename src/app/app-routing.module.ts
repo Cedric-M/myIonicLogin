73b0f3d7b1e75b959fa90ad17520a394e2a24cf2
Cedric-M <cirdec95@gmail.com>
2019-06-17 17:02:23 +0200

    Add: generated ProfilePage

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: './home/home.module#HomePageModule' },
+  { path: 'profile-page', loadChildren: './profile-page/profile-page.module#ProfilePagePageModule' },
 ];
 
 @NgModule({
